2916fe66698def0ce7591e949b8bd685375fe09e
Crypt0 <84045971+Crypt00o@users.noreply.github.com>
2021-11-26 02:04:08 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#RED="\e[31m"
+ORANGE="\e[33m"
+#BLUE="\e[94m"
+#GREEN="\e[92m"
+STOP="\e[0m"
+BLACK="\e[90m"
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+BLUE="\e[94m"
+PURPLE="\e[95m"
+CYAN="\e[96m"
+WHITE="\e[97m"
+
+# Checking Device Arch 
+printf "${BLUE}"
+printf "=========================================\n"
+printf "${GREEN}"
+echo "
+ __  __      ____                    
+|  \/  | ___|___ \ _   _  ___  _   _ 
+| |\/| |/ _ \ __) | | | |/ _ \| | | |
+| |  | |  __// __/| |_| | (_) | |_| |
+|_|  |_|\___|_____|\__, |\___/ \__,_|
+                   |___/             "
+printf "${ORANGE}"
+printf "\neslam,s Me2you Chat App "
+printf "${RED}"; echo "    +01095456354"
+printf "${BLUE}"
+printf "========================================="
+echo ' ';echo ' '
+printf "${GREEN}"
+echo "[+] Checking Device Arch [+] "
+printf "${YELLOW}"
+for i in {1..50} 
+do sleep 0.05 ;printf "."
+done
+printf "${GREEN}"
+printf " [%%100] "
+sleep 2
+start(){
+machine=$(uname -m);
+if [ $machine == "x86_64" ] 
+then cp linux/amd64/* . ;chmod +x client ;chmod +x server;
+elif [ $machine == "i686" ] 
+then cp linux/i686/* . ;chmod +x client ;chmod +x server;
+elif [ $machine == "aarch64" ] 
+then cp linux/aarch64/* . ;chmod +x client ;chmod +x server;
+fi
+}
+start
+printf "${YELLOW}"
+printf "\n\t\t[+] Done [+] "
+printf "${STOP}"
